7b67fe9e47a36cf8461d543ee448720a0a8e5f55
trivedisatvik <satvikftp99@gmail.com>
2022-03-31 23:30:06 +0530

    Deployment

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
+const path = require("path")
 
 
 const cors = require("cors");
