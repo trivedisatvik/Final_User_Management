f58d6a94d5e00dfaf564d9434dec7c9466542ded
trivedisatvik <satvikftp99@gmail.com>
2022-04-01 00:04:29 +0530

    Deployment

--- fynd-final-project-1/src/views/SignUpPage.vue
+++ fynd-final-project-1/src/views/SignUpPage.vue
@@ -129,7 +129,7 @@ else{
  }
  </script>
  <style scoped>
- @import url('https://fonts.googleapis.com/css2?family=Poppins:200,300,400,500,600,700,800,900&display=swap');
+
 *{
     margin :0;
     padding:0;
